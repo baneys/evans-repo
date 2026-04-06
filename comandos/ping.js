563704fd37214b87e3f2bf3568618495aa29a0b1
Baneys <emanuelrdr@outlook.com.br>
2026-04-06 13:19:40 -0300

    🦕 Update ping.js
    🦜 This update adds the /ping command for immediate testing and previews of future commands.

--- comandos/ping.js
+++ comandos/ping.js
@@ -1,1 +1,11 @@
+const { SlashCommandBuilder } = require('discord.js');
 
+module.exports = {
+  data: new SlashCommandBuilder()
+    .setName('ping')
+    .setDescription('see the latency'),
+  async execute(interaction, client) {
+    const ping = client.ws.ping;
+    await interaction.reply(`🏓 This is my latency: ${ping}**ms**`);
+  }
+}
